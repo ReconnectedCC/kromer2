d4390ecb36a5e8d4421b380e73474f82a17c8d3c
laincy <laincy@proton.me>
2026-01-23 10:40:11 -0500

    feat(tools): added flake and compose Added a flake and docker compose to help with dev, will probably cherry pick these out later if sov doesn't let me upstream them.

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils/";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    rust-overlay,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
+
+      toolchain = pkgs.rust-bin.stable.latest;
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "kromer2 dev";
+
+        packages = with pkgs; [
+          toolchain.default
+          sqlx-cli
+          just
+        ];
+
+        RUST_BACKTRACE = "full";
+      };
+    });
+}
